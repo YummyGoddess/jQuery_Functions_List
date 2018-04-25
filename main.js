2e4b5fb007d233ec6029a30a21abaabf064e69da
Danna E Wroblewski <danna.basschic@gmail.com>
2018-04-24 21:48:12 -0700

    Hide now works

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 $(document).ready(function(){
 
   $('#button1').click(function(){
-    alert("Did you click my button?");
+
+    $(".functions1 h2").text("Did you click my button?");
   });
 
+
+
+
+  $('#button2').click(function(){
+    $(".functions2 h2").hide();
+  });
+
+
+
+// All code must stay inside of this!
 });
